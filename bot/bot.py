daf2152c3e6181d40e71503dfe76ed2b177f00e9
Cedryk <cedryk.doucet@polymtl.ca>
2018-10-06 11:27:48 -0400

    fix point?

--- bot/bot.py
+++ bot/bot.py
@@ -27,7 +27,7 @@ class Bot:
                     house = Point(x,y)
         if not self.goingToHouse:
             minDist = float('inf')
-            closestRes = None
+            closestRes = Point()
             for resource in res:
                 dist = Point.Distance(resource, self.PlayerInfo.Position)
                 if dist < minDist:
@@ -38,7 +38,9 @@ class Bot:
                 return create_collect_action(direction)
             if self.PlayerInfo.TotalResources >= self.PlayerInfo.CarryingCapacity:
                 self.goingToHouse = True
-            if direction.x < direction.y:
+        else:
+            direction = house - self.PlayerInfo.Position
+        if direction.x < direction.y:
                 return create_move_action(Point(0, direction.y))
             else:
                 return create_move_action(Point(direction.x, 0))
