30351e7d237e89c4fc1e9b084a3317680a8bc3cc
Cedryk <cedryk.doucet@polymtl.ca>
2018-10-06 11:22:14 -0400

    sa compile po

--- bot/bot.py
+++ bot/bot.py
@@ -10,6 +10,7 @@ class Bot:
             :param playerInfo: Your bot's current state.
         """
         self.PlayerInfo = playerInfo
+        self.goingToHouse = False
 
     def execute_turn(self, gameMap, visiblePlayers):
         """
@@ -18,26 +19,29 @@ class Bot:
             :param visiblePlayers:  The list of visible players.
         """
         res = list()
-        for x in gameMap.xMax:
-            for y in gameMap.yMax:
+        for x in range(gameMap.xMax):
+            for y in (gameMap.yMax):
                 if gameMap.getTileAt(Point(x,y)) == 4:
                     res.append(Point(x,y))
                 elif gameMap.getTileAt(Point(x,y)) == 2:
                     house = Point(x,y)
-        minDist = float('inf')
-        closestRes = None
-        for resource in res:
-            dist = Point.Distance(resource, self.PlayerInfo.Position)
-            if dist < minDist:
-                minDist = dist
-                closestRes = resource
-        direction = closestRes - self.PlayerInfo.Position
-        if Point.Distance(self.PlayerInfo.Position, closestRes):
-            return create_collect_action(direction)
-        if direction.x < direction.y:
-            return create_move_action(Point(0, direction.y))
-        else:
-            return create_move_action(Point(direction.x, 0))
+        if not self.goingToHouse:
+            minDist = float('inf')
+            closestRes = None
+            for resource in res:
+                dist = Point.Distance(resource, self.PlayerInfo.Position)
+                if dist < minDist:
+                    minDist = dist
+                    closestRes = resource
+            direction = closestRes - self.PlayerInfo.Position
+            if Point.Distance(self.PlayerInfo.Position, closestRes):
+                return create_collect_action(direction)
+            if self.PlayerInfo.TotalResources >= self.PlayerInfo.CarryingCapacity:
+                self.goingToHouse = True
+            if direction.x < direction.y:
+                return create_move_action(Point(0, direction.y))
+            else:
+                return create_move_action(Point(direction.x, 0))
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
         return create_move_action(Point(1, 0))
 
